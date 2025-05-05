d03871542c07158d5bf3bc0df0ca442249102e1f
dvirvar <dvirv52@gmail.com>
2025-05-05 20:38:47 +0300

    - Detecting drag gestures

--- composeApp/src/commonMain/kotlin/com/skellyapps/charts/App.kt
+++ composeApp/src/commonMain/kotlin/com/skellyapps/charts/App.kt
@@ -2,6 +2,7 @@ package com.skellyapps.charts
 
 import androidx.annotation.FloatRange
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -20,7 +21,6 @@ import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
@@ -28,6 +28,7 @@ import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastForEachIndexed
 import androidx.compose.ui.util.fastMap
 import androidx.compose.ui.util.fastMaxOfOrNull
+import androidx.compose.ui.util.fastMinByOrNull
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.math.abs
 import kotlin.math.max
@@ -37,7 +38,8 @@ import kotlin.random.Random
 private const val blueTag = 0.toByte()
 private const val yellowTag = 1.toByte()
 private const val greenTag = 2.toByte()
-private val colors = listOf(Color.Blue, Color.Yellow, Color.Green)
+private const val redTag = 3.toByte()
+private val colors = listOf(Color.Blue, Color.Yellow, Color.Green, Color.Red)
 private val blueLine = LineChartData.Line(
     (1..75).map { LineChartData.Line.Point(it * Random.nextDouble(5.0, 15.0), Random.nextDouble(0.0, 100.0)) }.sortedBy { it.x },
     blueTag,
@@ -52,6 +54,20 @@ private val greenLine = LineChartData.Line(
     LineChartData.Line.Customization(colors[greenTag.toInt()])
 )
 private val leftAxis = LineChartData.HorizontalAxis(lines = listOf(blueLine, yellowLine, greenLine))
+private val redLine = LineChartData.Line(
+    (1..100).map { LineChartData.Line.Point(it * Random.nextDouble(5.0, 15.0), Random.nextDouble(0.0, 100.0)) }.sortedBy { it.x },
+    redTag,
+    LineChartData.Line.Customization(colors[redTag.toInt()])
+)
+private val rightAxis = LineChartData.HorizontalAxis(lines = listOf(redLine))
+
+private val dragAfterLongPress = object: LineChartData.DragAfterLongPress(closeOffset = { offset ->
+    abs(x - offset.x) <= 10 && abs(y - offset.y) <= 10
+}) {
+    override fun pointDraggedAfterLongPress(index: Int, lineTag: Byte, offset: Offset) {
+        println("I: $index, T: $lineTag, O: $offset")
+    }
+}
 
 @Composable
 @Preview
@@ -60,21 +76,27 @@ fun App() {
         LineChart(
             Modifier.fillMaxSize(),
             LineChartData(
-                leftAxis = leftAxis
-            )) { index, offset, lineTag ->
-            drawCircle(
-                colors[lineTag.toInt()],
-                radius = 5f,
-                offset
-            )
-        }
+                leftAxis = leftAxis,
+                rightAxis = rightAxis
+            ),
+            { index, offset, lineTag ->
+                drawCircle(
+                    colors[lineTag.toInt()],
+                    radius = 5f,
+                    offset
+                )
+            },
+            dragAfterLongPress
+        )
     }
 }
 
 @Composable
-private fun LineChart(modifier: Modifier,
-                      data: LineChartData,
-                      onEachPoint: (DrawScope.(index: Int, offset: Offset, lineTag: Byte) -> Unit)? = null
+private fun LineChart(
+    modifier: Modifier,
+    data: LineChartData,
+    onEachPoint: (DrawScope.(index: Int, offset: Offset, lineTag: Byte) -> Unit)? = null,
+    dragAfterLongPress: LineChartData.DragAfterLongPress? = null
 ) {
     var canvasSize by remember { mutableStateOf(IntSize(0,0)) }
     val minXValue by derivedStateOf {
@@ -117,24 +139,39 @@ private fun LineChart(modifier: Modifier,
             }, it.tag, it.customization)
         }
     }
-    var pointDragIndex by remember { mutableStateOf(-1) }
+    var draggedPoint by remember { mutableStateOf<DraggedPoint?>(null) }
 
     Canvas(modifier
         .onSizeChanged {
             canvasSize = it
         }
         .pointerInput(Unit) {
-//            detectDragGesturesAfterLongPress(onDragStart = { offset ->
-//                pointDragIndex = points.indexOfFirst {
-//                    it.closeTo(offset)
-//                }
-//            }, onDragEnd = {pointDragIndex = -1}, onDragCancel = {pointDragIndex = -1}) { change, offset ->
-//                if (pointDragIndex == -1) return@detectDragGesturesAfterLongPress
-//                val point = points[pointDragIndex]
-//                val newPoint = Offset(point.x + offset.x, point.y + offset.y)
-//                val temp = points.apply { set(pointDragIndex, newPoint) }.toMutableList()
-//                points = temp
-//            }
+            if (dragAfterLongPress != null) {
+                detectDragGesturesAfterLongPress(onDragStart = { offset ->
+                    val draggedPointsDistance = mutableListOf<DraggedPointDistance>()
+                    if (leftOffsetLines != null) {
+                        leftOffsetLines!!.fastForEach {
+                            val dpd = it.getClosestPointIfInRange(offset, dragAfterLongPress.closeOffset)
+                            if (dpd != null) {
+                                draggedPointsDistance.add(dpd)
+                            }
+                        }
+                    }
+                    if (rightOffsetLines != null) {
+                        rightOffsetLines!!.fastForEach {
+                            val dpd = it.getClosestPointIfInRange(offset, dragAfterLongPress.closeOffset)
+                            if (dpd != null) {
+                                draggedPointsDistance.add(dpd)
+                            }
+                        }
+                    }
+                    draggedPoint = draggedPointsDistance.fastMinByOrNull { it.distance }?.draggedPoint
+                }, onDragEnd = {draggedPoint = null}, onDragCancel = {draggedPoint = null}) { change, offset ->
+                    if (draggedPoint != null) {
+                        dragAfterLongPress.pointDraggedAfterLongPress(draggedPoint!!.index, draggedPoint!!.lineTag, offset)
+                    }
+                }
+            }
         }) {
         // Draw the line connecting the points
         if (leftOffsetLines != null) {
@@ -178,10 +215,6 @@ private fun LineChart(modifier: Modifier,
             }
         }
     }
-}
-
-private fun Offset.closeTo(offset: Offset): Boolean {
-    return abs(x - offset.x) <= 10 && abs(y - offset.y) <= 10
 }
 
 data class LineChartData(
@@ -250,7 +283,43 @@ data class LineChartData(
         val offsets: List<Offset>,
         val tag: Byte,
         val customization: Line.Customization
-    )
+    ) {
+        internal fun getClosestPointIfInRange(touchPoint: Offset, isInRange: Offset.(Offset) -> Boolean): DraggedPointDistance? {
+            if (offsets.isEmpty()) {
+                return null
+            }
+            val index = offsets.binarySearchBy(touchPoint.x) {
+                it.x
+            }
+            var closestIndexDistance: Pair<Int, Float>? = null
+            if (index < 0) {
+                var absIndex = abs(index + 1)
+                if (absIndex < offsets.size) {
+                    if (isInRange(offsets[absIndex], touchPoint)) {
+                        val distance = (offsets[absIndex] - touchPoint).getDistanceSquared()
+                        if (closestIndexDistance == null || closestIndexDistance.second > distance) {
+                            closestIndexDistance = Pair(absIndex, distance)
+                        }
+                    }
+                }
+                if (absIndex != 0) {
+                    absIndex -= 1
+                    if (isInRange(offsets[absIndex], touchPoint)) {
+                        val distance = (offsets[absIndex] - touchPoint).getDistanceSquared()
+                        if (closestIndexDistance == null || closestIndexDistance.second > distance) {
+                            closestIndexDistance = Pair(absIndex, distance)
+                        }
+                    }
+                }
+            } else {
+                closestIndexDistance = Pair(index, 0f)
+            }
+            if (closestIndexDistance != null) {
+                return DraggedPointDistance(DraggedPoint(closestIndexDistance.first, tag), closestIndexDistance.second)
+            }
+            return null
+        }
+    }
 
     open class Axis
     class HorizontalAxis(
@@ -262,4 +331,20 @@ data class LineChartData(
         val min: Int,
         val max: Int
     )
+
+    abstract class DragAfterLongPress(
+        val closeOffset: Offset.(Offset) -> Boolean
+    ) {
+        abstract fun pointDraggedAfterLongPress(index: Int, lineTag: Byte, offset: Offset)
+    }
 }
+
+internal data class DraggedPoint(
+    val index: Int,
+    val lineTag: Byte
+)
+
+internal data class DraggedPointDistance(
+    val draggedPoint: DraggedPoint,
+    val distance: Float
+)
